3c89f5cb7281c164df75aebf8a5ed4ad9c893e3f
JonatanSG95 <jonatansg95@gmail.com>
2024-08-13 22:55:33 -0300

    para el laburo

--- ventana.py
+++ ventana.py
@@ -1,4 +1,4 @@
-from tkinter import*
+from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from dados import dados_base
@@ -7,6 +7,11 @@ ventana_principal = Tk()
 ventana_principal.title("Calculadora")
 ventana_principal.minsize(width=800, height=600)
 ventana_principal.config(padx= 25, pady=25)
+
+
+mensaje = Label(ventana_principal, text='Elija que dado tirar')
+
+mensaje.pack()
 
 def mostrar_seleccionado():
     elegido = lista.get()
@@ -19,7 +24,7 @@ lista = ttk.Combobox(
     state="readonly",
     values=dados_base
 )
-lista.set("Elija un dado")
+lista.set(dados_base[0])
 lista.place(x=50, y=50)
 
 boton = ttk.Button(text="Elegir", command=mostrar_seleccionado)
